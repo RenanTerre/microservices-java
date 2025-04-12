7ec27fa475d52b3b0d700e7a1a98fa6c485e9d35
Renan Terre <1136179@atitus.edu.br>
2025-04-11 21:48:15 -0300

    aula07

---
+++ sreating-service/src/main/java/br/edu/atitus/greating_service/SreatingServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.greating_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreatingServiceApplication.class, args);
+	}
+
+}
